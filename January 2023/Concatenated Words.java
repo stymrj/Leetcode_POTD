311d1bec9685bfb7f92d19062b6e5c4981d0803d
Satyam Raj <82960606+stymrj@users.noreply.github.com>
2023-01-27 19:06:44 +0530

    Create Concatenated Words.java

---
+++ January 2023/Concatenated Words.java
@@ -0,0 +1,86 @@
+/*
+Given an array of strings words (without duplicates), return all the concatenated words in the given list of words.
+
+A concatenated word is defined as a string that is comprised entirely of at least two shorter words in the given array.
+
+ 
+
+Example 1:
+
+Input: words = ["cat","cats","catsdogcats","dog","dogcatsdog","hippopotamuses","rat","ratcatdogcat"]
+Output: ["catsdogcats","dogcatsdog","ratcatdogcat"]
+Explanation: "catsdogcats" can be concatenated by "cats", "dog" and "cats"; 
+"dogcatsdog" can be concatenated by "dog", "cats" and "dog"; 
+"ratcatdogcat" can be concatenated by "rat", "cat", "dog" and "cat".
+Example 2:
+
+Input: words = ["cat","dog","catdog"]
+Output: ["catdog"]
+ 
+
+Constraints:
+
+1 <= words.length <= 104
+1 <= words[i].length <= 30
+words[i] consists of only lowercase English letters.
+All the strings of words are unique.
+1 <= sum(words[i].length) <= 105
+*/
+
+// Approach 1: Dynamic Programming
+
+class Solution {
+    public List<String> findAllConcatenatedWordsInADict(String[] words) {
+        final Set<String> dictionary = new HashSet<>(Arrays.asList(words));
+        final List<String> answer = new ArrayList<>();
+        for (final String word : words) {
+            final int length = word.length();
+            final boolean[] dp = new boolean[length + 1];
+            dp[0] = true;
+            for (int i = 1; i <= length; ++i) {
+                for (int j = (i == length ? 1 : 0); !dp[i] && j < i; ++j) {
+                    dp[i] = dp[j] && dictionary.contains(word.substring(j, i)); 
+                }
+            }
+            if (dp[length]) {
+                answer.add(word);
+            }
+        }
+        return answer;   
+    }
+}
+
+// Approach 2: DFS
+
+class Solution {
+    private boolean dfs(final String word, int length, final boolean[] visited, final Set<String> dictionary) {
+        if (length == word.length()) {
+            return true;
+        }
+        if (visited[length]) {
+            return false;
+        }
+        visited[length] = true;
+        for (int i = word.length() - (length == 0 ? 1 : 0); i > length; --i) {
+            if (dictionary.contains(word.substring(length, i)) 
+                && dfs(word, i, visited, dictionary)) {
+                return true;
+            }
+            
+        }
+        return false;
+    }
+    
+    public List<String> findAllConcatenatedWordsInADict(String[] words) {
+        final Set<String> dictionary = new HashSet<>(Arrays.asList(words));
+        final List<String> answer = new ArrayList<>();
+        for (final String word : words) {
+            final int length = word.length();
+            final boolean[] visited = new boolean[length];
+            if (dfs(word, 0, visited, dictionary)) {
+                answer.add(word);
+            }
+        }
+        return answer;   
+    }
+}
