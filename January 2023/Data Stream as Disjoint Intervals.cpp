2cd06dc399a0a8983e41d5b5a9ac5a6aae1605f4
Satyam Raj <82960606+stymrj@users.noreply.github.com>
2023-01-28 17:24:56 +0530

    Create Data Stream as Disjoint Intervals.cpp

---
+++ January 2023/Data Stream as Disjoint Intervals.cpp
@@ -0,0 +1,75 @@
+/*
+Given a data stream input of non-negative integers a1, a2, ..., an, summarize the numbers seen so far as a list of disjoint intervals.
+
+Implement the SummaryRanges class:
+
+SummaryRanges() Initializes the object with an empty stream.
+void addNum(int value) Adds the integer value to the stream.
+int[][] getIntervals() Returns a summary of the integers in the stream currently as a list of disjoint intervals [starti, endi]. The answer should be sorted by starti.
+ 
+
+Example 1:
+
+Input
+["SummaryRanges", "addNum", "getIntervals", "addNum", "getIntervals", "addNum", "getIntervals", "addNum", "getIntervals", "addNum", "getIntervals"]
+[[], [1], [], [3], [], [7], [], [2], [], [6], []]
+Output
+[null, null, [[1, 1]], null, [[1, 1], [3, 3]], null, [[1, 1], [3, 3], [7, 7]], null, [[1, 3], [7, 7]], null, [[1, 3], [6, 7]]]
+
+Explanation
+SummaryRanges summaryRanges = new SummaryRanges();
+summaryRanges.addNum(1);      // arr = [1]
+summaryRanges.getIntervals(); // return [[1, 1]]
+summaryRanges.addNum(3);      // arr = [1, 3]
+summaryRanges.getIntervals(); // return [[1, 1], [3, 3]]
+summaryRanges.addNum(7);      // arr = [1, 3, 7]
+summaryRanges.getIntervals(); // return [[1, 1], [3, 3], [7, 7]]
+summaryRanges.addNum(2);      // arr = [1, 2, 3, 7]
+summaryRanges.getIntervals(); // return [[1, 3], [7, 7]]
+summaryRanges.addNum(6);      // arr = [1, 2, 3, 6, 7]
+summaryRanges.getIntervals(); // return [[1, 3], [6, 7]]
+ 
+
+Constraints:
+
+0 <= value <= 104
+At most 3 * 104 calls will be made to addNum and getIntervals.
+ 
+
+Follow up: What if there are lots of merges and the number of disjoint intervals is small compared to the size of the data stream?
+*/
+class SummaryRanges {
+public:
+//APPROACH 1
+
+    // taking a set to only have the element just once so if any duplicates comes it only 
+    //take it one time
+
+    // time complexity -- set is ordered so insertion takes O(logn) so n elements take O(nlogn)
+    // and we are running a for loop So O(n) there so total is O(n)+O(nlogn)
+    set<int>st;
+    SummaryRanges() {
+        st.clear();
+    }
+    
+    void addNum(int value) {
+        st.insert(value);
+    }
+    
+    vector<vector<int>> getIntervals() {
+        
+        vector<int> range(begin(st),end(st));
+         int n=range.size();
+        vector<vector<int>>result;
+        for(int i=0;i<n;i++){
+            int left = range[i];
+            while(i<n-1 && range[i]+1==range[i+1]){
+                i++;
+            }
+            result.push_back({left,range[i]});
+
+        }
+        return result;
+    }
+
+};
