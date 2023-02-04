c0eba22870a98fe4511c967d5f18d5de29aae0db
Satyam Raj <82960606+stymrj@users.noreply.github.com>
2023-02-04 21:06:17 +0530

    Create 567. Permutation in String.java

---
+++ February 2023/567. Permutation in String.java
@@ -0,0 +1,45 @@
+/*
+Given two strings s1 and s2, return true if s2 contains a permutation of s1, or false otherwise.
+In other words, return true if one of s1's permutations is the substring of s2.
+Example 1:
+Input: s1 = "ab", s2 = "eidbaooo"
+Output: true
+Explanation: s2 contains one permutation of s1 ("ba").
+Example 2:
+Input: s1 = "ab", s2 = "eidboaoo"
+Output: false
+Constraints:
+1 <= s1.length, s2.length <= 104
+s1 and s2 consist of lowercase English letters.
+*/
+
+public class Solution {
+    boolean flag = false;
+    
+    public boolean checkInclusion(String s1, String s2) {
+        permute(s1, s2, 0);
+        return flag;
+    }
+    
+    public String swap(String s, int i0, int i1) {
+        if (i0 == i1)
+            return s;
+        String s1 = s.substring(0, i0);
+        String s2 = s.substring(i0 + 1, i1);
+        String s3 = s.substring(i1 + 1);
+        return s1 + s.charAt(i1) + s2 + s.charAt(i0) + s3;
+    }
+    
+    void permute(String s1, String s2, int l) {
+        if (l == s1.length()) {
+            if (s2.indexOf(s1) >= 0)
+                flag = true;
+        } else {
+            for (int i = l; i < s1.length(); i++) {
+                s1 = swap(s1, l, i);
+                permute(s1, s2, l + 1);
+                s1 = swap(s1, l, i);
+            }
+        }
+    }
+}
